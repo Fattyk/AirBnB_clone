5facca894eb9774424d7b3aa3335c5ea5c1cabaf
Fattyk <ogundele.fatai.k@gmail.com>
2022-09-13 06:00:42 -0700

    Authors List

--- generate-authors.sh
+++ generate-authors.sh
@@ -1,8 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
-echo
-git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf > AUTHORS
+# see also ".mailmap" for how email addresses and names are deduplicated
 
+{
+    cat <<EOF
+-'EOH'
+	# This file lists all individuals having contributed content to the repository.
+	# For how it is generated, see `hack/generate-authors.sh`.
+	EOH
+	echo
+	git log --format='%aN <%aE>' | sort -uf
+} > AUTHORS
+
+EOF
